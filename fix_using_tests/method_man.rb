eaf05e3437db67f91888ae8e4ceecc62cb9f2bb0
Derek Turner <turner.derek.b@gmail.com>
2020-05-27 22:28:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,8 @@
 # don't forget to add: require 'pry'
 require'pry'
 def start_game(player1, player2)
-  print "Hello #{player1} & #{playar2}"
+  message = "Hello #{player1} & #{playar2}"
+  message
 end
 
 
