fe9a07700148ca69549ef5a635874c60b8a6f01e
Derek Turner <turner.derek.b@gmail.com>
2020-05-27 22:34:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,5 +1,6 @@
 # don't forget to add: require 'pry'
 require 'pry'
+
 def snake_it_up(string)
   if string[0] == "s"
     10 * "s" + string
@@ -7,4 +8,6 @@ def snake_it_up(string)
     string
     binding.pry
   end
+  
 end
+snake_it_up
