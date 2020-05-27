e3266bcfa3d43ad883d68617fe72e16ffd687fc5
Derek Turner <turner.derek.b@gmail.com>
2020-05-27 22:19:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,12 +1,10 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  puts "Hello #{player1} & #{playar2}"
-
-   
+  "Hello #{player1} & #{playar2}"
 end
 
-
+start_game
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
